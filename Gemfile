45ef93cb825e9c729f4f306b46a008c9d4291599
sonicpacer <werkbook@gmail.com>
2014-06-10 03:03:32 +0800

    Added devise

--- Gemfile
+++ Gemfile
@@ -19,6 +19,7 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'bootstrap-sass'
+gem 'devise', '~> 3.2.4'
 
 group :development,  :test do
 	gem 'sqlite3'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+ 
+
+ #note to set this to your actual host
+ #required for Heroku
+  config.action_mailer.default_url_options = { :host => 'http://spc-pinteresting.herokuapp.com/' }
 end
